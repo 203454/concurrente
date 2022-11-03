be5a027690dc12bf61d06905afeb7994a2e66964
Ciro Diaz <77992720+203454@users.noreply.github.com>
2022-11-03 11:04:48 -0600

    ACTIVIDAD 5

---
+++ ACTIVIDAD 5/Mesero.py
@@ -0,0 +1,39 @@
+from threading import Thread
+from time import sleep
+
+class Mesero(Thread):
+    def __init__(self, conditionM, cocinero, order):
+        Thread.__init__(self)
+        self.conditionM = conditionM
+        self.cocinero = cocinero
+        self.comida = False
+        self.order = order
+        
+    def attend(self, client):
+        self.conditionM.acquire()
+        print(f"-Mesero: tomando la orden del cliente {client}")
+        self.order = [client, False]
+        self.sendOrderToCook(client)
+        sleep(10)
+        self.conditionM.notify()
+        self.conditionM.release()
+        return self.order
+            
+    def sendOrderToCook(self, client):
+          self.conditionM.acquire()
+          print(f"-Mesero: llevando la orden de {client} al concinero")
+          sleep(5)
+          self.cocinero.cocinar(client)
+          sleep(2)
+          self.conditionM.notify()
+          self.conditionM.release()
+          print("-Mesero: Descansando")
+          
+    def llevarComida(self, client):
+        self.conditionM.acquire()
+        sleep(5)
+        self.conditionM.notify()
+        self.conditionM.release()
+        self.order = ["", False]
+        print("-Mesero: Descansando")
+        return True
